69db9b03e01d73a7a87bbed3925db9dbac62f1f3
sardorxonakramov <sardorxonakramov01@gmail.com>
2025-08-06 03:17:11 +0500

    fixed by Users's admin

--- Users/admin.py
+++ Users/admin.py
@@ -1,51 +1,26 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
-from django.utils.translation import gettext_lazy as _
+
 from .models import User
-from .forms import CustomUserCreationForm, CustomUserChangeForm
 
 
 @admin.register(User)
 class UserAdmin(BaseUserAdmin):
-    form = CustomUserChangeForm
-    add_form = CustomUserCreationForm
     model = User
-
-    list_display = (
-        "id", "phone", "email", "first_name", "last_name", "role",
-        "is_active", "is_staff") #"created_at", "updated_at"
-    # )
-    list_filter = ("role", "is_active", "is_staff", "is_superuser")
-    search_fields = ("phone", "email", "first_name", "last_name", "passport_series")
     ordering = ("-id",)
-
-    readonly_fields = ("created_at", "updated_at", "created_by", "updated_by", "last_login")
-
+    list_display = ("id", "phone", "email", "first_name", "last_name", "role", "is_active", "is_staff", "date_joined")
+    list_filter = ("is_active", "is_staff", "is_superuser", "role")
+    search_fields = ("phone", "email", "first_name", "last_name", "passport_series")
+    readonly_fields = ("date_joined",)
     fieldsets = (
         (None, {"fields": ("phone", "email", "password")}),
-        (_("Personal info"), {
-            "fields": ("first_name", "last_name", "passport_series")
-        }),
-        (_("Permissions"), {
-            "fields": (
-                "role", "is_active", "is_staff", "is_superuser",
-                "groups", "user_permissions"
-            )
-        }),
-        (_("Important dates"), {
-            "fields": ("last_login", "created_at", "updated_at", "created_by", "updated_by")
-        }),
+        ("Shaxsiy ma'lumotlar", {"fields": ("first_name", "last_name", "passport_series")}),
+        ("Ruxsatlar", {"fields": ("is_active", "is_staff", "is_superuser", "groups", "user_permissions", "role")}),
+        ("Tizim ma'lumotlari", {"fields": ("last_login", "date_joined")}),
     )
-
     add_fieldsets = (
         (None, {
             "classes": ("wide",),
-            "fields": (
-                "phone", "email", "password1", "password2",
-                "first_name", "last_name", "passport_series", "role",
-                "is_active", "is_staff", "is_superuser"
-            ),
+            "fields": ("phone", "email", "password1", "password2", "role", "is_active", "is_staff", "is_superuser"),
         }),
     )
-
-    filter_horizontal = ("groups", "user_permissions")
--- Users/models.py
+++ Users/models.py
@@ -84,3 +84,11 @@ class User(AbstractBaseUser, PermissionsMixin, BaseModel):
 
     def __str__(self):
         return self.phone
+    def get_full_name(self):
+        if self.first_name and self.last_name:
+            return f"{self.first_name} {self.last_name}"
+        elif self.first_name:
+            return self.first_name
+        elif self.last_name:
+            return self.last_name
+        return self.phone  # fallback, agar ismlar yo'q bo'lsa
